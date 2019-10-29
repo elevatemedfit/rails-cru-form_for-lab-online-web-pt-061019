e57bcdf5247bfd3b701efa9ddd06f18cf5e855a2
Chris Junker <elevatemedfit@gmail.com>
2019-10-29 17:11:36 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,25 +1,25 @@
 class ArtistsController < ApplicationController
 
 	def show
-		@artist = ArtistClass.find(params[:id])
+		@artist = Artist.find(params[:id])
 	end
 
 	def new
-		@artist = ArtistClass.new
+		@artist = Artist.new
 	end
 
 	def edit
-		@artist = ArtistClass.find(params[:id])
+		@artist = Artist.find(params[:id])
 	end
 
 	def create
-  @artist = ArtistClass.new(artist_params(:name, :bio))
+  @artist = Artist.new(artist_params(:name, :bio))
   @artist.save
   redirect_to artist_path(@artist)
 end
 
 def update
-  @artist = ArtistClass.find(params[:id])
+  @artist = Artist.find(params[:id])
   @artist.update(artist_params(:title, :room_number))
   redirect_to artist_path(@artist)
 end
@@ -28,7 +28,7 @@ private
 
 
 # We pass the permitted fields in as *args;
-# this keeps `ArtistClasses_params` pretty dry while
+# this keeps `Artistes_params` pretty dry while
 # still allowing slightly different behavior
 # depending on the controller action. This
 # should come after the other methods
