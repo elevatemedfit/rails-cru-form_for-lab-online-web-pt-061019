36247825f7471aa36f20fcb1fd7c7ddeb36a5f92
Chris Junker <elevatemedfit@gmail.com>
2019-10-29 17:04:25 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,39 @@
 class ArtistsController < ApplicationController
+
+	def show
+		@artist = ArtistClass.find(params[:id])
+	end
+
+	def new
+		@artist = ArtistClass.new
+	end
+
+	def edit
+		@artist = ArtistClass.find(params[:id])
+	end
+
+	def create
+  @artist = ArtistClass.new(artist_params(:name, :bio))
+  @artist.save
+  redirect_to artist_path(@artist)
+end
+
+def update
+  @artist = ArtistClass.find(params[:id])
+  @artist.update(artist_params(:title, :room_number))
+  redirect_to artist_path(@artist)
+end
+
+private
+
+
+# We pass the permitted fields in as *args;
+# this keeps `ArtistClasses_params` pretty dry while
+# still allowing slightly different behavior
+# depending on the controller action. This
+# should come after the other methods
+
+def artist_params(*args)
+  params.require(:artist_class).permit(*args)
+end
 end
